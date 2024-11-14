64c2f69d24299ebf273dd6ffa673360b9e566aa8
Ananya-Bhardwaj <ananya.bhar@gmail.com>
2024-11-14 22:45:48 +0530

    Files for user table added
    Added the schema, routes and controllers for user

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-// index.js
+import userRoute from './routes/User'
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use('/api/user', userRoute); 
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
